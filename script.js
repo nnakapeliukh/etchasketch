aaa971e6f435b895185e0986bc363f07762cfbbc
Nazar <nnakapeliukh@gmail.com>
2020-02-17 23:41:40 -0600

    added event listeners, color change for greyscale

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //temp
-let globalWidth = 5;
-let globalHeight = 5;
+let globalWidth = 25;
+let globalHeight = globalWidth;
 
 //create outer div
 let containerOuter = document.createElement("DIV");
@@ -14,14 +14,17 @@ function initGrid (gridHeight, gridWidth){
     containerOuter.style.gridTemplateRows = `repeat(${gridHeight} 1fr`;
     //style
     containerOuter.style.width = "50%";
-    containerOuter.style.height = '50%';    
+    containerOuter.style.height = '80%';    
+    containerOuter.style.minWwidth = "650px";
+    containerOuter.style.minHeight = '650px';    
+    
     containerOuter.style.position = "absolute";
     containerOuter.style.margin = "auto";
     containerOuter.style.top = "0";
     containerOuter.style.bottom = "0";
     containerOuter.style.left = "0";
     containerOuter.style.right = "0";
-    containerOuter.style.border = "3px solid red";
+    containerOuter.style.border = "3px solid silver";
     let t= document.getElementById('containerOuter').offsetHeight;
     containerOuter.style.width = t + "px";
 }
@@ -29,15 +32,19 @@ function initGrid (gridHeight, gridWidth){
 //creates a cell at row 'x' column 'y'
 function createCell (x,y){
     let cellNew = document.createElement("DIV");
-    
+    cellNew.colorBackgroundHex = 0;
     //cellNew.style.display = "inline-block";   
     cellNew.style.border = "1px solid grey";
     cellNew.style.gridRow = `${x}`;
     cellNew.style.gridColumn = `${y}`;
 
     cellNew.style.backgroundColor = "white"; 
-    cellNew.style.width = "100%"
-    cellNew.style.height = "100%"
+    cellNew.style.width = "100%";
+    cellNew.style.height = "100%";
+    cellNew.addEventListener("mouseover", (event) => { 
+        changeCellColor(cellNew, event)});
+    cellNew.addEventListener("mouseleave", (event) => { 
+        changeCellColor(cellNew, event)});
     containerOuter.appendChild(cellNew);
     return cellNew;
 }
@@ -50,4 +57,28 @@ for (let clmn =1; clmn<=globalHeight; clmn++){
        
     }
     
+}
+
+let greyColorsPool = ["#FFF", "#DDD", "#BBB","#999","#777", "#555", "#333", "#111"]
+function changeCellColor (inputElement, event)
+{
+    inputElement.style.backgroundColor =  greyColorsPool[inputElement.colorBackgroundHex]
+    inputElement.colorBackgroundHex++;
+    inputElement.style.transform = ("scale(1.1, 1.1)");
+    inputElement.style.transition = "0.1s";
+    if (event.type == "mouseover"){
+        inputElement.style.backgroundColor =  greyColorsPool[inputElement.colorBackgroundHex]
+        inputElement.colorBackgroundHex++;
+
+        inputElement.style.transition = "0.01s";
+        inputElement.style.transform = ("scale(1.1, 1.1)");
+        inputElement.style.zIndex = ("1")
+    }
+    else if (event.type == "mouseleave"){
+
+        inputElement.style.transition = "0.01s";
+        inputElement.style.transform = ("scale(1.0, 1.0)");
+
+        inputElement.style.zIndex = ("0")
+    }
 }
